9032fc98572d7e2b212ca89c1c0d129620341638
gabriel <gabrielboltron@gmail.com>
2019-06-13 17:32:16 +0800

    Add project files.

---
+++ G.CalendarSample/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace G.CalendarSample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.calendar1 = new G.Calendar.Calendar();
+            this.SuspendLayout();
+            // 
+            // calendar1
+            // 
+            this.calendar1.AllowEditingEvents = false;
+            this.calendar1.BackColor = System.Drawing.Color.White;
+            this.calendar1.CalendarDate = new System.DateTime(2019, 6, 13, 17, 29, 42, 526);
+            this.calendar1.CalendarView = G.Calendar.CalendarViews.Month;
+            this.calendar1.DateHeaderFont = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold);
+            this.calendar1.DayOfWeekFont = new System.Drawing.Font("Arial", 10F);
+            this.calendar1.DaysFont = new System.Drawing.Font("Arial", 10F);
+            this.calendar1.DayViewFont = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Bold);
+            this.calendar1.DimDisabledEvents = true;
+            this.calendar1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.calendar1.HighlightCurrentDay = true;
+            this.calendar1.LoadPresetHolidays = true;
+            this.calendar1.Location = new System.Drawing.Point(0, 0);
+            this.calendar1.MinimumSize = new System.Drawing.Size(512, 440);
+            this.calendar1.Name = "calendar1";
+            this.calendar1.ShowArrowControls = true;
+            this.calendar1.ShowDailyButton = true;
+            this.calendar1.ShowDashedBorderOnDisabledEvents = true;
+            this.calendar1.ShowDateInHeader = true;
+            this.calendar1.ShowDisabledEvents = false;
+            this.calendar1.ShowEventTooltips = false;
+            this.calendar1.ShowMonthButton = true;
+            this.calendar1.ShowTodayButton = true;
+            this.calendar1.Size = new System.Drawing.Size(736, 458);
+            this.calendar1.TabIndex = 0;
+            this.calendar1.TodayFont = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Bold);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(736, 458);
+            this.Controls.Add(this.calendar1);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "G Calendar Sample";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Calendar.Calendar calendar1;
+    }
+}
+
---
+++ G.CalendarSample/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows.Forms;
+using G.Calendar.Classes;
+
+namespace G.CalendarSample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            var ce = new CustomEvent
+            {
+                IgnoreTimeComponent = true,
+                EventText = "Eat! Sleep! Code!",
+                EventDetails = new { Welcome = "Hello" },
+                Date = DateTime.Now,
+                RecurringFrequency = RecurringFrequencies.None,
+                Enabled = true
+            };
+            calendar1.AddEvent(ce);
+
+            var he = new HolidayEvent
+            {
+                IgnoreTimeComponent = true,
+                EventText = "Codes Day!",
+                EventDetails = new { Hello = "World" },
+                Date = DateTime.Now,
+                RecurringFrequency = RecurringFrequencies.None,
+                Enabled = true
+            };
+            calendar1.AddEvent(he);
+        }
+    }
+}
